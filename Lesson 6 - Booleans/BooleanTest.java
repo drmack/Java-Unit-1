dd41785cf58fac481a932160b310f8ba330febf7
SageMoon4702 <33138040+SageMoon4702@users.noreply.github.com>
2019-09-04 13:47:02 -0400

    9-4-2019 U1L6 Added
    Short lesson, not much to booleans till you compare them and all that.

---
+++ Lesson 6 - Booleans/BooleanTest.java
@@ -0,0 +1,7 @@
+public class BooleanTest
+{
+    public static void main(String[] args) {
+        boolean isStudent = false;
+        System.out.println("Is student: " + isStudent);
+    }
+}
---
+++ Lesson 6 - Booleans/IceCream.java
@@ -0,0 +1,6 @@
+public class IceCream
+{
+    public static void main(String[] args) {
+        // Write your code here.
+    }
+}
